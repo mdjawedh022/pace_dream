b76ea494fc9bcdd9e88e23a27423d68a228651a9
Md Jawed <mdjawedh22@gmail.com>
2024-01-04 14:39:09 +0530

    template ready.

---
+++ index.js
@@ -0,0 +1,25 @@
+const express=require("express");
+const {connection} =require("./utils/db");
+const cors=require('cors');
+const { log } = require("console");
+require("dotenv").config();
+
+const app=express();
+
+app.get('/',(req,res)=>{
+    res.send("welecom to paceDream Hotel backend api!");
+
+});
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(process.env.port,async()=>{
+    try{
+   await connection
+   console.log("connected to the database")
+    }catch(err){
+        console.log("Database connection failed")
+    }
+    console.log(`server is running at the port ${process.env.port}`)
+})
