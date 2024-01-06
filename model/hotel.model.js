75fd8ed7ba67fddfabb43def72e56622b8492409
Md Jawed <mdjawedh22@gmail.com>
2024-01-06 14:43:33 +0530

    task complete

--- model/hotel.model.js
+++ model/hotel.model.js
@@ -1,71 +1,65 @@
-const mongoose=require("mongoose");
+const mongoose = require("mongoose");
 
 const hotelSchema = mongoose.Schema(
   {
     name: { type: String, required: true },
     image: [{ type: String, required: true }],
     description: { type: String, required: true },
-    offered: [
-      {
-        free_parking: { type: Boolean, required: true },
-        tv: { type: Boolean, required: true },
-        dedicated_workspace: { type: Boolean, required: true },
-        air_conditioning: { type: Boolean, required: true },
-        wifi: { type: Boolean, required: true },
-        washing_machine: { type: Boolean, required: true },
-      },
-    ],
-    standout_amenities: [
-      {
-        exercise_equipment: { type: Boolean, required: true },
-        patio: { type: Boolean, required: true },
-        bbq_grill: { type: Boolean, required: true },
-        outdoor_shower: { type: Boolean, required: true },
-        pool: { type: Boolean, required: true },
-        hot_tub: { type: Boolean, required: true },
-        patio: { type: Boolean, required: true },
-        fire_fit: { type: Boolean, required: true },
-      },
-    ],
-    describes_your_place: [
-      {
-        hotel: { type: Boolean, required: true },
-        apartment: { type: Boolean, required: true },
-        house: { type: Boolean, required: true },
-        flat: { type: Boolean, required: true },
-      },
-    ],
-    room_status: [
-      {
-        entire_house: { type: Boolean, required: true },
-        shared_room: { type: Boolean, required: true },
-        single_room: { type: Boolean, required: true },
-      },
-    ],
-    gender: [
-      {
-        male: { type: Boolean, required: true },
-        female: { type: Boolean, required: true },
-        both_can_stay: { type: Boolean, required: true },
-      },
-    ],
+    offered: [{
+      free_parking: { type: Boolean, default: false },
+      tv: { type: Boolean, default: false },
+      dedicated_workspace: { type: Boolean, default: false },
+      air_conditioning: { type: Boolean, default: false },
+      wifi: { type: Boolean, default: false },
+      washing_machine: { type: Boolean, default: false },
+    }],
+    standout_amenities: [{
+      exercise_equipment: { type: Boolean, default: false },
+      patio: { type: Boolean, default: false },
+      bbq_grill: { type: Boolean, default: false },
+      outdoor_shower: { type: Boolean, default: false },
+      pool: { type: Boolean, default: false },
+      hot_tub: { type: Boolean, default: false },
+      fire_pit: { type: Boolean, default: false },
+    }],
+    describes_your_place: {
+      hotel: { type: Boolean, default: false },
+      apartment: { type: Boolean, default: false },
+      house: { type: Boolean, default: false },
+      flat: { type: Boolean, default: false },
+    },
+    room_status: {
+      entire_house: { type: Boolean, default: false },
+      shared_room: { type: Boolean, default: false },
+      single_room: { type: Boolean, default: false },
+    },
+    gender: {type:String, enum:["male","female","both"], required:true},
     guests: { type: Number, required: true },
     bedrooms: { type: Number, required: true },
     beds: { type: Number, required: true },
     bathrooms: { type: Number, required: true },
-    hours: { type: Number },
-    days: [
+    price_by: [
       {
-        monday: { type: Number },
-        tuesday: { type: Number },
-        wednesday: { type: Number },
-        thursday: { type: Number },
-        friday: { type: Number },
-        saturday: { type: Number },
-        sunday: { type: Number },
+        day: {
+          type: String,
+          enum: [
+            "monday",
+            "tuesday",
+            "wednesday",
+            "thursday",
+            "friday",
+            "saturday",
+            "sunday",
+          ],
+        },
+        hours: { type: Number },
+        price: { type: Number, required: true },
       },
     ],
-    set_price: { type: Number, required: true },
+    set_price: {
+      date_range: [Date],
+      price: { type: Number, required: true },
+    },
     location: {
       type: { type: String, default: "Point" },
       coordinates: { type: [Number], required: true },
@@ -73,13 +67,16 @@ const hotelSchema = mongoose.Schema(
   },
   {
     versionKey: false,
+  },
+  {
+    timestamps: true,
   }
 );
+
 hotelSchema.index({ location: "2dsphere" });
 
+const HotelDetail = mongoose.model("hotel_details", hotelSchema);
 
-const hotelModel = mongoose.model("Hotel_Details", hotelSchema);
-
-module.exports={
-    hotelModel
-}
+module.exports = {
+  HotelDetail,
+};
--- routes/hotel.routes.js
+++ routes/hotel.routes.js
@@ -1,30 +1,63 @@
-const express=require("express");
-const { hotelModel } = require("../model/hotel.model");
+const express = require("express");
+const { hotelModel, HotelDetail } = require("../model/hotel.model");
 
+const hotelRouter = express.Router();
 
-const hotelRouter=express.Router();
-
-
-hotelRouter.post("/post",async(req,res)=>{
-    try{
-        
-        const {name,image,description,offered,standout_amenities,describes_your_place,room_status,gender,guests,bedrooms,beds,bathrooms,hours,days,set_price,location}=req.body;
-        const hotelData=new hotelModel({name,image,description,offered,standout_amenities,describes_your_place,room_status,gender,guests,bedrooms,beds,bathrooms,hours,days,set_price,location})
-        await hotelData.save()
-      res.send({msg:"data send successsfully!😊",hotelData})
-    }catch(err){
-        res.send({msg:"Internal server error","error":err})
-    }
-})
+hotelRouter.post("/post", async (req, res) => {
+  try {
+    const {
+      name,
+      image,
+      description,
+      offered,
+      standout_amenities,
+      describes_your_place,
+      room_status,
+      gender,
+      guests,
+      bedrooms,
+      beds,
+      bathrooms,
+      hours,
+      days,
+      set_price,
+      location,
+    } = req.body;
+    const hotelData = new HotelDetail(req.body
+    //   {
+    //   name,
+    //   image,
+    //   description,
+    //   offered,
+    //   standout_amenities,
+    //   describes_your_place,
+    //   room_status,
+    //   gender,
+    //   guests,
+    //   bedrooms,
+    //   beds,
+    //   bathrooms,
+    //   hours,
+    //   days,
+    //   set_price,
+    //   location,
+    // }
+    );
+    await hotelData.save();
+    res.send({ msg: "data send successsfully!😊", hotelData });
+  } catch (err) {
+    res.send({ msg: "Internal server error", error: err.message });
+  }
+});
 // ....................get hotel data............................
 
 hotelRouter.get("/", async (req, res) => {
   try {
-    const hotels = await hotelModel.find();
-     res.send(hotels);
+    const hotels = await HotelDetail.find();
+    res.send(hotels);
   } catch (error) {
-     res.send({ message: error.message });
+    res.send({ message: error.message });
   }
 });
 
-module.exports={hotelRouter}
+module.exports = { hotelRouter };
